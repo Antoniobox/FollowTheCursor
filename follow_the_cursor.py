1dfeb249e4ed9eeb02862ab2375e93867869697f
Antoniobox <antonioboxsanchez@gmail.com>
2023-10-06 12:32:11 +0200

    Uso de PokeApi y refactorizacion de parte del codigo

--- follow_the_cursor.py
+++ follow_the_cursor.py
@@ -1,21 +1,44 @@
+import urllib.request
+import json
 import pygame
+import random
+import os
+
+FPS = 60
 
 pygame.init()
 
 window = pygame.display.set_mode((800, 600))
+
+def getPokemonImg():
+    num_pokemon = random.randint(1, 800)
+    pokemon = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/' + str(num_pokemon) + '.png'
+    response = urllib.request.urlopen(pokemon)
+    file_name = "pokemon/"+str(num_pokemon)+".png"
+    urllib.request.urlretrieve(pokemon, file_name)
+    return file_name
+
+pokemon_name = getPokemonImg()
+pokemon = pygame.image.load(pokemon_name)
 
 def main():
     clock = pygame.time.Clock()
     running = True
     mouse_pos = pygame.mouse.get_pos()
     while running:
-        clock.tick(60)
+        clock.tick(FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
+                #Elimina todas las imagenes de los pokemon
+                ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+                os.system("rm " + pokemon_name)
                 running = False
-        window.fill((255, 255, 255))
+        window.fill(pygame.Color('white'))
         if pygame.mouse.get_focused():
            mouse_pos = pygame.mouse.get_pos()
+        window.blit(pokemon, (0, 0))
         pygame.display.flip()
 
 main()
+
+
